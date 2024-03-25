e76c5e82f74982b4e75f87d70abb08e305845e14
ThatLukeDev <luke@l2j.co.uk>
2024-03-25 15:10:58 +0000

    norms for vectors

--- kyber.hpp
+++ kyber.hpp
@@ -4,16 +4,34 @@
 #include <cstring>
 #include <cmath>
 #include <stdexcept>
+#include <vector>
 
 namespace kyber {
 	const int n = 256;
 	const int _n = 9;
 	const int q = 3329;
 
-	unsigned char* BytesToBits(unsigned char* B, unsigned int k) {
-		unsigned char* _B = (unsigned char*)malloc(k*8);
+	std::vector<unsigned char> BytesToBits(unsigned char* B, unsigned int k) {
+		std::vector<unsigned char> _B(k*8);
 
 		for (unsigned int i = 0; i < k; i++) {
+			_B[i*8+0] = (B[i] & 0b00000001) ? 1 : 0;
+			_B[i*8+1] = (B[i] & 0b00000010) ? 1 : 0;
+			_B[i*8+2] = (B[i] & 0b00000100) ? 1 : 0;
+			_B[i*8+3] = (B[i] & 0b00001000) ? 1 : 0;
+			_B[i*8+4] = (B[i] & 0b00010000) ? 1 : 0;
+			_B[i*8+5] = (B[i] & 0b00100000) ? 1 : 0;
+			_B[i*8+6] = (B[i] & 0b01000000) ? 1 : 0;
+			_B[i*8+7] = (B[i] & 0b10000000) ? 1 : 0;
+		}
+
+		return _B;
+	}
+
+	std::vector<unsigned char> BytesToBits(std::vector<unsigned char> B) {
+		std::vector<unsigned char> _B(B.size() * 8);
+
+		for (unsigned int i = 0; i < B.size(); i++) {
 			_B[i*8+0] = (B[i] & 0b00000001) ? 1 : 0;
 			_B[i*8+1] = (B[i] & 0b00000010) ? 1 : 0;
 			_B[i*8+2] = (B[i] & 0b00000100) ? 1 : 0;
@@ -49,6 +67,33 @@ namespace kyber {
 		}
 
 		throw std::invalid_argument("Given params to reduce do not have r % a in 0 < r < a");
+	}
+
+	double norm2(std::vector<double> w) {
+		double _w = 0;
+
+		for (double _wn : w) {
+			_w += _wn * _wn;
+		}
+		_w = std::sqrt(_w);
+
+		return _w;
+	}
+
+	double normInf(std::vector<double> w) {
+		double _wMax = 0;
+		bool change = false;
+
+		for (double _wn : w) {
+			double _wnC = reduce(_wn, q);
+
+			if (_wnC > _wMax || change == false) {
+				_wMax = _wnC;
+				change = true;
+			}
+		}
+
+		return _wMax;
 	}
 }
 
