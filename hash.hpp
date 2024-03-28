711853151497fb93fec30f1625faa9f7ad5d3c76
ThatLukeDev <luke@l2j.co.uk>
2024-03-28 07:25:44 +0000

    theta now bitwise

--- hash.hpp
+++ hash.hpp
@@ -10,31 +10,44 @@ namespace sha3 {
 		unsigned int w = 64;
 	};
 
+	unsigned char getBit(unsigned char b, unsigned char x) {
+		return (b & (1 << x)) >> (x);
+	}
+	unsigned char setBit(unsigned char b, unsigned char x, unsigned char v) {
+		return (b & ~(1 << x)) | (v << x);
+	}
+
 	unsigned char* keccak_f(unsigned char* state, params _p) {
 		unsigned int t = std::sqrt(_p.w);
 
-		unsigned char a[5][5][_p.w];
-		memcpy(a, state, _p.w * 25);
+		unsigned char a[5][5][t];
+		memcpy(a, state, t * 25);
 
-		// theta
+		// theta : xor parity of each neighbouring column respectively
 		{
-			unsigned char c[5][_p.w];
-			unsigned char d[5][_p.w];
+			unsigned char c[5][t];
+			unsigned char d[5][t];
 
 			for (int x = 0; x < 5; x++) {
 				for (int z = 0; z < _p.w; z++) {
-					c[x][z] = a[x][0][z] ^ a[x][1][z] ^ a[x][2][z] ^ a[x][3][z] ^ a[x][4][z];
+					c[x][z] = setBit(c[x][z/8], z%8, getBit(a[x][0][z/8], z%8)
+						^ getBit(a[x][1][z/8], z%8)
+						^ getBit(a[x][2][z/8], z%8)
+						^ getBit(a[x][3][z/8], z%8)
+						^ getBit(a[x][4][z/8], z%8));
 				}
 			}
 			for (int x = 0; x < 5; x++) {
 				for (int z = 0; z < _p.w; z++) {
-					d[x][z] = c[(x-1)%5][z] ^ c[(x+1)%5][(z-1)%_p.w];
+					d[x][z] = setBit(d[x][z/8], z%8, getBit(c[(x-1)%5][z/8], z%8)
+						^ getBit(c[(x+1)%5][((z-1)%_p.w)/8],((z-1)%_p.w)%8));
 				}
 			}
 			for (int x = 0; x < 5; x++) {
 				for (int y = 0; y < 5; y++) {
 					for (int z = 0; z < _p.w; z++) {
-						a[x][y][z] ^= d[x][z];
+						a[x][y][z] = setBit(a[x][y][z/8], z%8, getBit(d[x][z/8], z%8)
+							^ getBit(d[x][z/8], z%8));
 					}
 				}
 			}
