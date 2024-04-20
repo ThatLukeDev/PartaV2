4643747c6e6696f9693746edf7a9fcbc90ae279b
ThatLukeDev <luke@l2j.co.uk>
2024-04-20 14:14:33 +0100

    makefile

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 
 #include "kyber.hpp"
 #include "hash.hpp"
+#include "base.hpp"
 
 int main() {
+	unsigned char inArr[200] = {0};
+	sha3::keccak_f(inArr, sha3::params());
+	std::cout << base16::encode(inArr, 200) << std::endl;
 	std::cout << "Hello World!" << std::endl;
 
 	return 0;
---
+++ makefile
@@ -0,0 +1,2 @@
+parta: main.cpp aes.hpp hash.hpp kyber.hpp
+	g++ main.cpp -o parta.out
