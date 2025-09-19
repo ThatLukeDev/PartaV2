d5ec0e804350c6c334e111329cf025edb1bc4b34
ThatLukeDev <luke@l2j.co.uk>
2025-09-19 07:40:01 +0100

    modular power

--- src/ring.rs
+++ src/ring.rs
@@ -1,13 +1,62 @@
 /// A negcyclic polynomial ring type
 #[derive(Debug, Clone, Copy)]
-struct NegacyclicRing {
+pub struct NegacyclicRing {
     modulus: i32,
     exponent: u32
 }
 
 impl NegacyclicRing {
+    /// Creates a new negacyclic ring (form X^n + 1)_q from n and q.
+    pub fn new(n: u32, q: i32) -> Self {
+        Self {
+            modulus: q,
+            exponent: n
+        }
+    }
+
     /// Gives the size of the key, or 2 ^ exponent.
-    fn size(&self) -> i32 {
+    ///
+    /// ```
+    ///# use partav2::ring::*;
+    /// assert_eq!(
+    ///     NegacyclicRing::new(4, 23).size(),
+    ///     16 // 2 ^ 4
+    /// )
+    /// ```
+    pub fn size(&self) -> i32 {
         return (2_i32).pow(self.exponent);
+    }
+
+    /// Returns x ^ y with respect to the Negacyclic ring.
+    ///
+    /// Does not support a negative exponent.
+    ///
+    /// ```
+    ///# use partav2::ring::*;
+    /// assert_eq!(
+    ///     NegacyclicRing::new(4, 23).power(3, 4),
+    ///     12 // 3 ^ 4 mod 23
+    /// )
+    /// ```
+    pub fn power(&self, x: i32, y: i32) -> i32{
+        match y {
+            ..0 => panic!("termPower: y: Argument less than zero. Negative exponents are prohibited."),
+            0 => 1,
+            0.. => {
+                let mut working: i32 = 1;
+
+                let mut base: i32 = x % self.modulus;
+                let mut exp: i32 = y;
+                while exp > 0 {
+                    if exp % 2 == 1 {
+                        working = (working * base) % self.modulus;
+                    }
+                    exp /= 2;
+                    base = (base * base) % self.modulus;
+                }
+
+                working
+            }
+        }
     }
 }
