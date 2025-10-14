c75ebb0c68b94dad5f69089d0a3d72a32b2c4c14
ThatLukeDev <luke@l2j.co.uk>
2025-10-14 08:12:00 +0100

    basic benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-// use partav2::ring::*;
+use partav2::ring::*;
+use partav2::rlwe::*;
+
+use std::time::SystemTime;
 
 fn main() {
-    println!("Hello, world!");
+    const ITERATIONS: i32 = 10;
+    let start = SystemTime::now();
+
+    for _ in 0..ITERATIONS {
+        let ring = NegacyclicRing::new(9, 25601);
+
+        let (private1, public1) = RLWE::generate(ring);
+        let (key2, public2) = RLWE::respond(ring, public1);
+        let key1 = RLWE::parse(ring, private1, public2);
+
+        assert_eq!(key1, key2);
+    }
+
+    println!("{}ms per keyshare", start.elapsed().unwrap().as_millis() as i32 / ITERATIONS);
 }
