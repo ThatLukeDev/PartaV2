d1c6b170d43136d36d9f7ba870e9d8beeaa6e822
ThatLukeDev <luke@l2j.co.uk>
2025-10-21 08:40:13 +0100

    frame for u8 based key exchangd

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 use crate::ring::NegacyclicRing;
+use crate::rlwe::*;
 
 /// Contains representation of negacyclic polynomial rings, and utility functions
 pub mod ring;
@@ -19,8 +20,8 @@ pub enum Security {
     Medium,
     /// Equivelent to 256-bit RSA
     High,
-    /// You must make sure that the exponent and modulus form a negacyclic ring with a 2n-th root
-    /// of unity
+    /// You must make sure that the exponent and modulus form a negacyclic ring
+    /// with a 2n-th root of unity
     Custom(u32, i32)
 }
 
@@ -47,4 +48,78 @@ impl Security {
             }
         )
     }
+}
+
+/// Generates a new request keypair for ring learning with errors
+///
+/// request(security) -> (private, request)
+///
+///```
+/// let (private1, public1) = partav2::request(partav2::Security::High);
+///```
+pub fn request(level: Security) -> (Vec<u8>, Vec<u8>) {
+    let ring = level.ring();
+    let (private1, public1) = RLWE::generate(ring);
+
+    let mut public = vec![];
+    public.extend(ring.modulus.to_le_bytes());
+    public.extend(ring.exponent.to_le_bytes());
+    public.extend(public1.to_bytes());
+
+    (private1.to_bytes(), public)
+}
+
+/// Generates a response keypair for ring learning with errors
+///
+/// respond(request) -> (key, response)
+///
+///```
+///#let (private1, public1) = partav2::request(partav2::Security::High);
+/// let (key2, public2) = partav2::respond(public1);
+///```
+pub fn respond(request: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
+    let ring = NegacyclicRing::new(u32::from_le_bytes(request[4..8].try_into().unwrap()), i32::from_le_bytes(request[0..4].try_into().unwrap()));
+    let mut splice = request;
+    splice.drain(0..8);
+
+    let public = PublicKeypair::from_bytes(splice);
+
+    let (key1, response) = RLWE::respond(ring, public);
+
+    let mut key = vec![];
+
+    for i in 0..key1.len() {
+        key.extend(key1[i].to_le_bytes());
+    }
+
+    (key, response.to_bytes())
+}
+
+/// Parses the response keypair
+///
+/// finalise(rprivate, response) -> key
+///
+///```
+/// let (private1, public1) = partav2::request(partav2::Security::High);
+/// let (key2, public2) = partav2::respond(public1);
+/// let key1 = partav2::respond(private1, public2);
+///
+/// assert_eq!(key1, key2);
+///```
+pub fn finalise(private: Vec<u8>, request: Vec<u8>) -> Vec<u8> {
+    let ring = NegacyclicRing::new(u32::from_le_bytes(request[4..8].try_into().unwrap()), i32::from_le_bytes(request[0..4].try_into().unwrap()));
+    let mut splice = request;
+    splice.drain(0..8);
+
+    let public = PublicKeypair::from_bytes(splice);
+
+    let key1 = RLWE::parse(ring, PrivateKeypair::from_bytes(private), public);
+
+    let mut key = vec![];
+
+    for i in 0..key1.len() {
+        key.extend(key1[i].to_le_bytes());
+    }
+
+    key
 }
